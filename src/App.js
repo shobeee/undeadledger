a160f213ca7f945f961decbf1fb4d771d2c8b847
Shoaib <shoaibdarktr94@gmail.com>
2024-03-20 07:28:17 +0500

    second-push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App content"> 
+    {/* <nav className="navbar navbar-expand-lg bg-transparent opacity-75">
+  <div className="container-fluid">
+    <a className="navbar-brand text-danger" href="/">Undead-Ledger</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active text-danger" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-danger" href="/">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle text-danger" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item text-danger" >Action</a></li>
+            <li><a className="dropdown-item text-danger" >Another action</a></li>
+            <li><a className="dropdown-item text-danger" >Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled text-danger" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success text-danger" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav> */}
+<div className='name'>
+<h1 className='text-light h1'>Undead Ledger</h1>
+<span className='text-light h2'>Survival Saga</span>
+</div>  
+
+
     </div>
   );
 }
